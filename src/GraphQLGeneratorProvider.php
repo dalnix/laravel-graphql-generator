64237363e87190d1c0d04256e8a8bca8c3cf021c
Christian Janbjer <christian@dalnix.se>
2018-09-10 20:58:14 +0200

    Possible fix for #1

--- src/GraphQLGeneratorProvider.php
+++ src/GraphQLGeneratorProvider.php
@@ -1,84 +1,86 @@
 <?php namespace Dalnix\GraphQLGenerator;
 
-use GraphQL\Type\SchemaConfig;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\ServiceProvider;
 use Dalnix\GraphQLGenerator\Command\GenerateType;
 use Folklore\GraphQL\Support\Facades\GraphQL;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
 
-class GraphQLGeneratorProvider extends ServiceProvider
-{
-    protected $files;
-    public function __construct(Filesystem $files) {
-        parent::__construct();
-        $this->files = $files;
-    }
-    public function register()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                GenerateType::class
-            ]);
-        }
-    }
+class GraphQLGeneratorProvider extends ServiceProvider {
+	
+	public function register() {
+		if ( $this->app->runningInConsole() ) {
+			$this->commands( [
+				GenerateType::class
+			] );
+		}
+	}
 
-    public function getModels($dir, $namespace)
-    {
-        $dir = $dir;
-        $files = scandir($dir);
+	public function getModels( $dir, $namespace ) {
+		$dir   = $dir;
+		$files = scandir( $dir );
 
-        $models = array();
-        $namespace = $namespace;
+		$models    = array();
+		$namespace = $namespace;
 
-        foreach ($files as $file) {
-            //skip current and parent folder entries and non-php files
-            if ($file == '.' || $file == '..' || !strpos($file, '.php')) {
-                continue;
-            }
-            $models[] = $namespace . substr($file, 0, -4);
-        }
+		foreach ( $files as $file ) {
+			//skip current and parent folder entries and non-php files
+			if ( $file == '.' || $file == '..' || ! strpos( $file, '.php' ) ) {
+				continue;
+			}
+			$models[] = $namespace . substr( $file, 0, - 4 );
+		}
 
-        return $models;
-    }
+		return $models;
+	}
 
-    public function boot()
-    {
+	public function boot() {
 
-        $this->publishes([
-            __DIR__ . '/config/graphql_generator.php' => config_path('graphql_generator.php'),
-        ]);
-        if (!$this->files->isDirectory('app/GraphQL')) {
-            $this->files->makeDirectory('app/GraphQL');
-        }
-        $types =  $this->getModels(app_path("") . '/GraphQL/Type', 'App\\GraphQL\\Type\\');
-        $mutations =  $this->getModels(app_path("") . '/GraphQL/Mutation', 'App\\GraphQL\\Mutation\\');
-        $queries =  $this->getModels(app_path("") . '/GraphQL/Query', 'App\\GraphQL\\Query\\');
-        $correct_types = [];
-        $correct_mutations = [];
+		$this->publishes( [
+			__DIR__ . '/config/graphql_generator.php' => config_path( 'graphql_generator.php' ),
+		] );
+		if ( ! is_dir('app/GraphQL' ) ) {
+			Storage::makeDirectory( 'app/GraphQL' );
+		}
+		if ( ! is_dir('app/GraphQL/Type')) {
+			Storage::makeDirectory( 'app/GraphQL/Type' );
+		}
+		if ( ! is_dir('app/GraphQL/Mutation')) {
+			Storage::makeDirectory( 'app/GraphQL/Mutation' );
+		}
+		if ( ! is_dir('app/GraphQL/Query')) {
+			Storage::makeDirectory( 'app/GraphQL/Query' );
+		}
+		$types             = $this->getModels( app_path( "" ) . '/GraphQL/Type', 'App\\GraphQL\\Type\\' );
+		$mutations         = $this->getModels( app_path( "" ) . '/GraphQL/Mutation', 'App\\GraphQL\\Mutation\\' );
+		$queries           = $this->getModels( app_path( "" ) . '/GraphQL/Query', 'App\\GraphQL\\Query\\' );
+		$correct_types     = [ ];
+		$correct_mutations = [ ];
 
-        foreach ($types as $type) {
-            $class_type = new $type();
-            $attr = $class_type->getAttributes();
-            $correct_types[str_replace('App\\GraphQL\\Type\\', '', $type)] = $type;
-        }
+		foreach ( $types as $type ) {
+			$class_type                                                        = new $type();
+			$attr                                                              = $class_type->getAttributes();
+			$correct_types[ str_replace( 'App\\GraphQL\\Type\\', '', $type ) ] = $type;
+		}
 
-        foreach ($mutations as $m) {
-            $class_type = new $m();
-            $correct_mutations[] = $m;
-        }
-        $correct_queries = [];
-        foreach ($queries as $q) {
-            $class_type = new $q();
-            $correct_queries[] = $q;
-        }
+		foreach ( $mutations as $m ) {
+			$class_type          = new $m();
+			$correct_mutations[] = $m;
+		}
+		$correct_queries = [ ];
+		foreach ( $queries as $q ) {
+			$class_type        = new $q();
+			$correct_queries[] = $q;
+		}
 
-        $schema = GraphQL::schema([
-            'types' => $correct_types,
-            'query' => $correct_queries,
-            'mutation' => $correct_mutations
-        ]);
+		$schema = GraphQL::schema( [
+			'types'    => $correct_types,
+			'query'    => $correct_queries,
+			'mutation' => $correct_mutations
+		] );
 
-        GraphQL::addSchema(config('graphql_generator')['schema_name'], ['query' => $correct_queries,'mutation' => $correct_mutations]);
+		GraphQL::addSchema( config( 'graphql_generator' )['schema_name'],
+			[ 'query' => $correct_queries, 'mutation' => $correct_mutations ] );
 
-    }
+	}
 }
